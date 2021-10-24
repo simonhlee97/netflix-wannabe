7b767bfe79432952a145130bc5ded9d15142f426
Simon Lee <simonhlee97@hotmail.com>
2021-10-24 15:25:39 +0900

    fix: delete un-needed files

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,20 +1,20 @@
-import Footer from '../components/footer'
-import { useRouter } from 'next/router'
+// import Footer from '../components/footer'
+// import { useRouter } from 'next/router'
 
-export default function Welcome() {
-	const router = useRouter()
-	const { email } = router.query
+// export default function Welcome() {
+// 	const router = useRouter()
+// 	const { email } = router.query
 
-	return (
-		<>
-			<main>
-				<p>
-					Thank you for signing up. Please check your {email} inbox to verify your e-mail
-					address!
-				</p>
-			</main>
+// 	return (
+// 		<>
+// 			<main>
+// 				<p>
+// 					Thank you for signing up. Please check your {email} inbox to verify your e-mail
+// 					address!
+// 				</p>
+// 			</main>
 
-			<Footer />
-		</>
-	)
-}
+// 			<Footer />
+// 		</>
+// 	)
+// }
